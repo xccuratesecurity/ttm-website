796768654dd6f7c479a90460c951a125c83d1bac
jineshnagori <jineshn21@gmail.com>
2022-07-30 19:52:58 +0530

    final changes in home page

--- assets/script/index.js
+++ assets/script/index.js
@@ -18,28 +18,3 @@ $('.nav-item .nav-link').click(function (e) {
     $('.nav-item .nav-link').removeClass('active');
     $(this).addClass('active');
 });
-
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-});
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    stagePadding: 50,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:7
-        }
-    }
-})
