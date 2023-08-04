4ca95dcffe77beff0b198e6c9817edefb6dde4c4
xccuratesecurity <83909719+xccuratesecurity@users.noreply.github.com>
2023-08-04 19:23:54 +0530

    Update jquery.countdown.js

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -11,7 +11,6 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
 @version: 1.0.1
 */
 
-
 (function() {
 
   (function($) {
@@ -111,7 +110,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       return this.init();
     };
     $.countdown.defaultOptions = {
-      date: "June 7, 2087 15:03:25",
+      date: "August 7, 2023 00:00:00",
       refresh: 1000,
       onEnd: $.noop,
       render: function(date) {
